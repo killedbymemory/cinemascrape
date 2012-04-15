c14e03be71429ede264db3ee5277c750ef33f38e
Leonardo Situmorang <leonardo@situmorang.net>
2012-04-15 20:53:55 +0700

    added cinema21 request handler cinema21.js will handle request such as 'now playing', 'list city', etc. it will called by apps.js

--- app.js
+++ app.js
@@ -28,11 +28,27 @@ app.configure('production', function(){
 });
 
 // Routes
+app.get('/cinema21', function(req, res){
+	var cinema21 = require('./cinema21');
+	console.log(cinema21);
 
-app.get('/', routes.index);
+	console.log(req.query);	
 
-/*
+	var action = req.param('action');
+
+	switch(action) {
+		case 'now-playing':
+			new cinema21(res).now_playing();
+			break;
+
+		case 'list-city':
+			output = 'list available city';
+			break;
+	}
+});
+
+//*
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
-#*/
+//*/
--- cinema21.js
+++ cinema21.js
@@ -10,29 +10,48 @@ var request_param = {
 	}
 };
 
-request(request_param, function(err, response, body){
-	var self = this;
+function cinema21(res) {
+	var _res;
+	this._res = res
+};
 
-	if (err && respose.statusCode == 200) {
-		console.log('Request error.');
-	}
+module.exports = cinema21;
 
-	// use jsdom to parse response body
-	// also to attach jquery the scripts
-	jsdom.env({
-		html:body,
-		scripts:['http://code.jquery.com/jquery-1.6.min.js'],
-	}, 
-	function(err, window){
-		var $ = window.jQuery;
-		var nowPlaying = [];
-		
-		$('#box_content ol:last li').each(function(){
-			console.log($('a', this).attr('href'));
-			$anchor = $('a', this);
-			nowPlaying.push([$anchor.html(), $anchor.attr('href')]);
+cinema21.prototype.now_playing = function() {
+	console.log('called...');
+	var _this = this;
+
+	request(request_param, function(err, response, body){
+		var self = this;
+
+		if (err && respose.statusCode == 200) {
+			console.log('Request error.');
+		}
+
+		// use jsdom to parse response body
+		// also to attach jquery the scripts
+		jsdom.env({
+			html:body,
+			scripts:['http://code.jquery.com/jquery-1.6.min.js'],
+		}, 
+		function(err, window){
+			var $ = window.jQuery;
+			var nowPlaying = [];
+			
+			$('#box_content ol:last li').each(function(){
+				console.log($('a', this).attr('href'));
+				$anchor = $('a', this);
+				nowPlaying.push([$anchor.html(), $anchor.attr('href')]);
+			});
+
+			var output = '';
+			$(nowPlaying).each(function(index, element){
+				output += nowPlaying[index].join(' - ') + '<br />';
+			});
+
+			console.log(output);
+			_this._res.end(output);
 		});
-
-		console.log(nowPlaying);
 	});
-});
+};
+
